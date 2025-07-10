0c630e5d4725b9d02e586e3239bc8c1c864e31ed
James <jahamezs123@gmail.com>
2025-07-10 16:15:58 +0800

    Second commit

--- Wiki/encyclopedia/urls.py
+++ Wiki/encyclopedia/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("css/", views.css, name="css"),
+    path("<str:title>/", views.page, name="entry"),
     
 ]
--- Wiki/encyclopedia/views.py
+++ Wiki/encyclopedia/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from markdown2 import Markdown
 from . import util
 
-def convery_md_to_html(title):
+def convert_md_to_html(title):
     markdowner = Markdown()
     content = util.get_entry(title)
     if content == None:
@@ -16,7 +16,14 @@ def index(request):
     })
 
 # try to change this to fit every md file, using <str:title>
-def css(request):
-    return render(request, "encyclopedia/css.html", {
-        "css_content": convery_md_to_html("CSS")})
+def page(request, title):
+    content = convert_md_to_html(title.lower())
+    if content is None:
+        return render(request, "encyclopedia/404.html", {
+            "title": title.lower()
+        }, status=404)
+    
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "content": content, "title": title.lower()})
 
