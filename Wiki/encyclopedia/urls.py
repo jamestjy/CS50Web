c8b9b30fe3a1524801a22323989dd9c20786a015
James <jahamezs123@gmail.com>
2025-07-11 20:47:20 +0800

    Third Commit

--- Wiki/encyclopedia/urls.py
+++ Wiki/encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # calling page function in views.py, passing in url title to views.page title
     path("<str:title>/", views.page, name="entry"),
+    path("search/", views.search, name="search"),
     
 ]
--- Wiki/encyclopedia/views.py
+++ Wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from markdown2 import Markdown
 from . import util
+import re
 
 def convert_md_to_html(title):
     markdowner = Markdown()
@@ -15,15 +16,43 @@ def index(request):
         "entries": util.list_entries()
     })
 
-# try to change this to fit every md file, using <str:title>
 def page(request, title):
-    content = convert_md_to_html(title.lower())
+    content = convert_md_to_html(title.lower()) #used lower() since title=entry from index.html contains caps
     if content is None:
+        # "title": title.lower()" passes in the lowercase title of the url
+        # to 404.html template, as variable called title(key)
         return render(request, "encyclopedia/404.html", {
             "title": title.lower()
         }, status=404)
     
     else:
+        # same action for title as above, but content is now passing in md
+        # converted to html
         return render(request, "encyclopedia/entry.html", {
             "content": content, "title": title.lower()})
 
+def search(request):
+    search_query = request.GET.get("q", "")
+    search_query = search_query.strip().lower()
+    matches = [entry for entry in util.list_entries() if re.search(search_query, entry.lower())]
+    if len(matches) == 1:
+        # if search_query matches an entry exactly, render that one entry.html page
+        return render(request, "encyclopedia/entry.html", {
+            "content": convert_md_to_html(matches[0])
+        })
+
+    elif len(matches) == 0:
+        # if no matches, return a 404 page
+        return render(request, "encyclopedia/404.html", {
+            "title": search_query
+        }, status=404)
+
+    else:
+        # if search_query is a substring of any entry,
+        # return a search results page displaying a list of all entries with 
+        # search_query as a substring
+        return render(request, "encyclopedia/search_results.html", {
+            "matches": matches, "search_query": search_query
+        })
+           
+    
